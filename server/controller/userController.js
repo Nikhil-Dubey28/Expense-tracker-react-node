48a9d2dbc6be5c8fb05c613d46628eeb8031026b
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-08-05 14:22:08 +0530

    bcrypt

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,17 +1,22 @@
 const User = require('../model/User')
+const bcrypt = require('bcrypt')
+
 
 // register/sign up 
 const createUser = async (req, res) => {
     try {
         const { name, email, password } = req.body
+        const salt = await bcrypt.genSalt(10)
+        const hashedPass = await bcrypt.hash(password, salt)
 
         const existingUser = await User.findOne({ where: { email } })
 
         if (existingUser) {
             res.status(409).json({ message: 'User with this email already exists' })
+            return
         }
 
-        const user = await User.create({ name, email, password })
+        const user = await User.create({ name, email, password:hashedPass })
         res.status(201).json(user)
 
     } catch (err) {
@@ -32,7 +37,7 @@ const login = async (req, res) => {
         if (!userDetails) {
             res.status(404).json('wrong email')
         } else {
-            const correctPass = userDetails.password === password
+            const correctPass = await bcrypt.compare( password,userDetails.password)
             if (correctPass) {
                 res.status(200).json('user login successful')
             } else {
