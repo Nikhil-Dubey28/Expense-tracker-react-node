1d1fc38ff0122eb26994e0c5ed25955faf5e4661
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-08-13 12:11:57 +0530

    reset password

--- server/controller/resetpassController.js
+++ server/controller/resetpassController.js
@@ -3,6 +3,8 @@ const Sib = require('sib-api-v3-sdk')
 
 const User =require('../model/User')
 
+const bcrypt = require('bcrypt')
+
 const Forgotpassword = require('../model/Forgotpassword')
 const config = require('dotenv').config
 config({path:'./config/config.env'})
@@ -57,6 +59,75 @@ const forgotPassword = async (req,res) => {
 }
 
 
+const resetPassword = async (req,res) => {
+    try{
+    const {id} =req.params
+   const forgotpasswordrequest = await Forgotpassword.findOne({where : {id}})
+
+   if(forgotpasswordrequest){
+    await forgotpasswordrequest.update({active: false})
+
+    res.status(200).send(`<html>
+                                    <script>
+                                        function formsubmitted(e){
+                                            e.preventDefault();
+                                            console.log('called')
+                                        }
+                                    </script>
+
+                                    <form action="/api/password/updatepassword/${id}" method="get">
+                                        <label for="newpassword">Enter New password</label>
+                                        <input name="newpassword" type="password" required></input>
+                                        <button>reset password</button>
+                                    </form>
+                                </html>`);
+    
+
+                                    }
+   }catch(err) {
+    console.log(err)
+    res.status(500).send('Internal server error')
+   }
+   res.end()
+}
+
+
+
+const updatePassword = async(req,res) => {
+    try{
+        const {newpassword} = req.query  
+        const {resetpasswordid} = req.params
+
+        const resetpasswordrequest = await Forgotpassword.findOne({where: {id:resetpasswordid}})
+        if (!resetpasswordrequest) {
+            return res.status(404).json({ error: 'Reset password request not found', success: false });
+        }
+
+        const user = await User.findOne({where: {id:resetpasswordrequest.userId}})
+        if (!user) {
+            return res.status(404).json({ error: 'No user exists', success: false });
+        }
+        const saltRounds = 10
+        const salt = await bcrypt.genSalt(saltRounds)
+        const hash = await bcrypt.hash(newpassword,salt)
+
+        await user.update({password: hash})
+
+        res.status(201).json({message: 'password successfully updated'})
+
+
+
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error: 'internal server error'})
+    }
+}
+
+
+
+
 module.exports = {
-    forgotPassword
+    forgotPassword,
+    resetPassword,
+    updatePassword
 }
